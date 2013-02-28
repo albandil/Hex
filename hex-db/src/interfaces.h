28d549275ab936aaea5fb6ffb73c8492822f74c5
Jakub Benda <jakub.benda@seznam.cz>
2013-02-28 17:49:37 +0100

    Reworked C/FORTRAN interfaces (unsupported at the moment).

---
+++ hex-db/src/interfaces.h
@@ -0,0 +1,235 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *\
+ *                                                                           *
+ *                       / /   / /    __    \ \  / /                         *
+ *                      / /__ / /   / _ \    \ \/ /                          *
+ *                     /  ___  /   | |/_/    / /\ \                          *
+ *                    / /   / /    \_\      / /  \ \                         *
+ *                                                                           *
+ *                         Jakub Benda (c) 2013                              *
+ *                     Charles University in Prague                          *
+ *                                                                           *
+\* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+#ifndef HEX_DB_INTERFACES_H
+#define HEX_DB_INTERFACES_H
+
+/**
+ * Create new database
+ */
+void create_new_database();
+
+/**
+ * Initialize
+ */
+void initialize(const char* dbname);
+
+/**
+ * Import SQL batch file.
+ * Equivalent to
+ * \code
+ *     sqlite3 "hex.db" < "batchfile.sql"
+ * \endcode
+ */
+void import(const char* sqlname);
+
+/**
+ * Update integral cross section.
+ * Used after import of T-matrices.
+ */
+void update();
+
+#if 0
+
+/**
+ * Return the scattering amplitude.
+ * \f[
+ *      f^S = -\frac{1}{2\pi} \sum_{\ell L} T^{LMS}_\ell Y_{l_f M-m_f}
+ * \f]
+ * \param ni Initial atomic principal quantum number.
+ * \param li Initial atomic orbital quantum number.
+ * \param mi Initial atomic magnetic quantum number.
+ * \param nf Final atomic principal quantum number.
+ * \param lf Final atomic principal quantum number.
+ * \param mf Final atomic magnetic quantum number.
+ * \param S Total spin (only needed for proper anti/symmetrization
+ *          of the initial wave function).
+ * \param Ei Projectile energy in Rydbergs (\f$ = k_i^2 \f$)
+ * \param N Array length.
+ * \param theta Polar angle of scattered electron.
+ * \param amplitudes Results (N complex numbers as 2N real numbers).
+ */
+extern "C" void scattering_amplitude (
+	int const * ni, int const * li, int const * mi,
+	int const * nf, int const * lf, int const * mf,
+	int const * S, double const * Ei,
+	int const * N, double const * theta, double * amplitudes
+);
+
+/**
+ * Compute the differential cross section.
+ * \f[
+ *       \frac{\mathrm{d}\sigma^S}{\mathrm{d}\Omega} = \frac{k_f}{k_i} \frac{2S+1}{4} |f^S|^2
+ * \f]
+ * \param ni Initial atomic principal quantum number.
+ * \param li Initial atomic orbital quantum number.
+ * \param mi Initial atomic magnetic quantum number.
+ * \param nf Final atomic principal quantum number.
+ * \param lf Final atomic principal quantum number.
+ * \param mf Final atomic magnetic quantum number.
+ * \param S Total spin (only needed for proper anti/symmetrization
+ *          of the initial wave function).
+ * \param N Array length.
+ * \param Ei Projectile energy in Rydbergs (\f$ = k_i^2 \f$)
+ * \param theta Polar angle of scattered electron.
+ * \param dsigma Results.
+ */
+extern "C" void differential_cross_section (
+	int const * ni, int const * li, int const * mi,
+	int const * nf, int const * lf, int const * mf,
+	int const * S, double const * Ei,
+	int const * N, double const * theta, double * dsigma
+);
+
+/**
+ * Compute the momentum transfer.
+ * \f[
+ *      \eta = \int \frac{\mathrm{d}\sigma}{\mathrm{d}\Omega} (1 - \cos\theta) \mathrm{d}\Omega(\hat{\vec{k}}_f)
+ *      = ...
+ * \f]
+ * \param ni Initial atomic principal quantum number.
+ * \param li Initial atomic orbital quantum number.
+ * \param mi Initial atomic magnetic quantum number.
+ * \param nf Final atomic principal quantum number.
+ * \param lf Final atomic principal quantum number.
+ * \param mf Final atomic magnetic quantum number.
+ * \param L Total angular momentum of the electrons.
+ * \param S Total spin (only needed for proper anti/symmetrization
+ *          of the initial wave function).
+ * \param N Array length.
+ * \param Ei Projectile energy in Rydbergs (\f$ = k_i^2 \f$)
+ * \param eta Results.
+ */
+extern "C" void momentum_transfer (
+	int const * ni, int const * li, int const * mi,
+	int const * nf, int const * lf, int const * mf,
+	int const * L, int const * S,
+	int const * N, double const * Ei, double * eta
+);
+
+/**
+ * Compute integral cross section \f$ \sigma^{LS} \f$,
+ * \f[
+ *      \sigma^{LS}(E) = \int_{4\pi} \frac{\mathrm{d}\sigma^S}{\mathrm{d}\Omega}
+ *      \mathrm{d}\Omega(\hat{\vec{k}}_f)
+ *      \Rightarrow
+ *      \sigma^{LS}(E) = \frac{k_f}{k_i} \frac{1}{4\pi^2} \frac{2S+1}{4} \sum_\ell \left|T_\ell^{LMS}\right|^2
+ * \f]
+ * \param ni Initial atomic principal quantum number.
+ * \param li Initial atomic orbital quantum number.
+ * \param mi Initial atomic magnetic quantum number.
+ * \param nf Final atomic principal quantum number.
+ * \param lf Final atomic principal quantum number.
+ * \param mf Final atomic magnetic quantum number.
+ * \param L Total angular momentum of the electrons.
+ * \param S Total spin (only needed for proper anti/symmetrization
+ *          of the initial wave function).
+ * \param N Array length.
+ * \param Ei Projectile energy in Rydbergs (\f$ = k_i^2 \f$)
+ * \param sigma Results.
+ */
+extern "C" void integral_cross_section (
+	int const * ni, int const * li, int const * mi,
+	int const * nf, int const * lf, int const * mf,
+	int const * L, int const * S,
+	int const * N, double const * Ei, double * sigma
+);
+
+/**
+ * Sum integral cross sections.
+ * \f[
+ *      \sigma_{i \rightarrow f}(E) = \sum_{LS} \sigma_{i \rightarrow f}^{LS}(E)
+ * \f]
+ * \param ni Initial atomic principal quantum number.
+ * \param li Initial atomic orbital quantum number.
+ * \param mi Initial atomic magnetic quantum number.
+ * \param nf Final atomic principal quantum number.
+ * \param lf Final atomic principal quantum number.
+ * \param mf Final atomic magnetic quantum number.
+ * \param N Array length.
+ * \param Ei Projectile energy in Rydbergs (\f$ = k_i^2 \f$).
+ * \param sigma Results.
+ */
+extern "C" void complete_cross_section (
+	int const * ni, int const * li, int const * mi,
+	int const * nf, int const * lf, int const * mf,
+	int const * N, double const * Ei, double * sigma
+);
+
+/**
+ * Sum integral cross sections and extrapolate \f$ L \rightarrow \infty \f$.
+ * \f[
+ *      \sigma_{i \rightarrow f}(E) = \sum_{LS} \sigma_{i \rightarrow f}^{LS}(E)
+ * \f]
+ * The extrapolation is done using Aitken Δ²-process. 
+ * \param ni Initial atomic principal quantum number.
+ * \param li Initial atomic orbital quantum number.
+ * \param mi Initial atomic magnetic quantum number.
+ * \param nf Final atomic principal quantum number.
+ * \param lf Final atomic principal quantum number.
+ * \param mf Final atomic magnetic quantum number.
+ * \param N Array length.
+ * \param Ei Projectile energy in Rydbergs (\f$ = k_i^2 \f$)
+ * \param sigma Results.
+ */
+extern "C" void extrapolate_cross_section (
+	int const * ni, int const * li, int const * mi,
+	int const * nf, int const * lf, int const * mf,
+	int const * N, double const * Ei, double * sigma
+);
+
+/**
+ * Compute the collision strength.
+ * \f[
+ *     \Omega_{i \rightarrow f}(E) = k_i^2 (2L+1) (2S+1) \sigma_{i \rightarrow f}(E)
+ * \f]
+ * \param ni Initial atomic principal quantum number.
+ * \param li Initial atomic orbital quantum number.
+ * \param mi Initial atomic magnetic quantum number.
+ * \param nf Final atomic principal quantum number.
+ * \param lf Final atomic principal quantum number.
+ * \param mf Final atomic magnetic quantum number.
+ * \param L Total angular momentum of the electrons.
+ * \param S Total spin (only needed for proper anti/symmetrization
+ *          of the initial wave function).
+ * \param N Array length.
+ * \param Ei Projectile energy in Rydbergs (\f$ = k_i^2 \f$)
+ * \param omega Results.
+ */
+extern "C" void collision_strength (
+	int const * ni, int const * li, int const * mi,
+	int const * nf, int const * lf, int const * mf,
+	int const * L, int const * S,
+	int const * N, double const * Ei, double * omega
+);
+
+/**
+ * Compute total cross section.
+ * \f[
+ *      \sigma_{i \rightarrow *}(E) = \sum_{n_f = 0}^\infty \sum_{l_f = 0}^{n_f - 1}
+ *             \sigma_{i \rightarrow f}(E)
+ * \f]
+ * \param ni Initial atomic principal quantum number.
+ * \param li Initial atomic orbital quantum number.
+ * \param mi Initial atomic magnetic quantum number.
+ * \param N Array length.
+ * \param Ei Projectile energy in Rydbergs (\f$ = k_i^2 \f$).
+ * \param sigma Results.
+ */
+extern "C" void total_cross_section (
+	int const * ni, int const * li, int const * mi,
+	int const * N, double const * Ei, double * sigma
+);
+
+#endif
+
+#endif
--- hex-db/src/variables.h
+++ hex-db/src/variables.h
@@ -21,6 +21,17 @@
 
 #include "arrays.h"
 
+/**
+ * \brief Convert dictionary entry to a numeric type.
+ * 
+ * Being given a dictionary (= string-string map) and a keyword,
+ * the function finds a correct entry and returns its value converted
+ * to the template datatype.
+ * \param dict Dictionary to search in.
+ * \param keyword Entry to look for.
+ * \param name Identification of the calling authority for use in error
+ *             message if the entry is not find.
+ */
 template <typename T> T As (
 	std::map<std::string,std::string> const & dict,
 	std::string const & keyword,
@@ -42,30 +53,55 @@ template <typename T> T As (
 	return x;
 }
 
+/**
+ * \brief Base class for scatering variables.
+ * 
+ * This is the heritage base for all scattering quantities that can be
+ * computed by hex-db.
+ */
 class Variable
 {
 public:
 	
-	// destructor
+	/// destructor
 	virtual ~Variable() {}
 	
 	// getters
+	
+	/// String identification of the variable.
 	virtual std::string const & id() const = 0;
+	
+	/// Longer description text for use in program help.
 	virtual std::string const & description() const = 0;
-	virtual std::string const & SQL_Update() const = 0;
+	
+	/// SQL statement that creates the required table, or empty string if not needed.
 	virtual std::string const & SQL_CreateTable() const = 0;
+	
+	/// SQL statement that updates the table after insetion of new data.
+	virtual std::string const & SQL_Update() const = 0;
+	
+	/// List of scattering event parameters that have to be specified by user.
 	virtual std::vector<std::string> const & dependencies() const = 0;
 
-	// write out requested data
+	// others
+	
+	/// write out requested data
 	virtual bool run (
 		sqlitepp::session & db,
 		std::map<std::string,std::string> const & data1,
 		rArray const & data2
 	) const = 0;
 	
+	/// Returns the program logo for use in output.
 	std::string logo() const;
 };
 
+/**
+ * \brief List of variables.
+ * 
+ * All available variables are elements of the list. They are being added in
+ * the constructor.
+ */
 class VariableList
 {
 public:
@@ -90,9 +126,27 @@ private:
 	std::vector<Variable*> list;
 };
 
+/**
+ * \brief Create class for a given variable name.
+ * 
+ * Creates class declaration so that only definition of the unique 
+ * members is necessary. This method allows easy addition of new
+ * scattering variables; one just needs to add the line
+ * \code
+ * AddNewVariableClass(NewVariableClassName);
+ * \endcode
+ * at the end of "variables.h", add a line
+ * \code
+ * new NewVariableClassName,
+ * \endcode
+ * among others into the constructor of VariableList in "variables.cpp"
+ * and implement the methods in a new CPP file, which is then linked
+ * to the program. It is not necessary to write a custom header which needs
+ * to be included by every other source file.
+ */
 #define AddNewVariableClass(ClassName) \
 	class ClassName : public Variable \
-	{ \
+{ \
 	public: \
 \
 		static const std::string Id; \
@@ -112,18 +166,36 @@ private:
 			std::map<std::string,std::string> const & data1, \
 			rArray const & data2 \
 		) const; \
-	};
+};
 
+/// Create class for T-matrix (tmat)
 AddNewVariableClass(TMatrix);
+
+/// Create class for scattering amplitude (scatamp)
 AddNewVariableClass(ScatteringAmplitude);
+
+/// Create class for differential cross section (dcs)
 AddNewVariableClass(DifferentialCrossSection);
+
+/// Create class for integral cross section (ics)
 AddNewVariableClass(IntegralCrossSection);
+
+/// Create class for complete cross section (ccs)
 AddNewVariableClass(CompleteCrossSection);
+
+/// Create class for extrapolated cross section (xcs)
 AddNewVariableClass(ExtrapolatedCrossSection);
+
+/// Create class for collision strength (colls)
 AddNewVariableClass(CollisionStrength);
+
+/// Create class for momentum transfer (momtf)
 AddNewVariableClass(MomentumTransfer);
+
+/// Create class for total cross section (tcs)
 AddNewVariableClass(TotalCrossSection);
-	
+
+/// forward declaration of the variable list "vlist"
 extern VariableList vlist;
 
 #endif
