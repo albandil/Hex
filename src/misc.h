64c3560e142be690432adebf38710e413b14ed36
Jakub Benda <jakub.benda@seznam.cz>
2013-04-05 19:28:33 +0200

    DB: Enable import of semicolon-separated SQL batch files.

--- src/misc.h
+++ src/misc.h
@@ -23,6 +23,31 @@
 /// Not-a-number
 #define Nan (std::numeric_limits<double>::quiet_NaN())
 
+#include <algorithm> 
+#include <functional> 
+#include <cctype>
+#include <locale>
+
+/// Trim from start.
+static inline std::string &ltrim(std::string &s)
+{
+	s.erase(s.begin(), std::find_if(s.begin(), s.end(), std::not1(std::ptr_fun<int, int>(std::isspace))));
+	return s;
+}
+
+/// Trim from end.
+static inline std::string &rtrim(std::string &s)
+{
+	s.erase(std::find_if(s.rbegin(), s.rend(), std::not1(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
+	return s;
+}
+
+/// Trim from both ends.
+static inline std::string &trim(std::string &s)
+{
+	return ltrim(rtrim(s));
+}
+
 /**
  * Custom exception class with easy printf-like constructor.
  */
