28a8e280b91395df926a4673f5e9b57b350e7fba
Jakub Benda <jakub.benda@seznam.cz>
2013-04-27 19:38:27 +0200

    New utility hdf2hdf for compression of Array<> HDFs.

---
+++ utilities/hdf2hdf/hdf2hdf.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+#include "../../src/arrays.h"
+#include "../../src/arrays.cpp"
+
+int main(int argc, char *argv[])
+{
+	std::vector<std::string> files;
+	bool compress = false;
+	
+	for (int iarg = 1; iarg < argc; iarg++)
+	{
+		if (strcmp(argv[iarg],"--compress") == 0)
+			compress = true;
+		else
+			files.push_back(std::string(argv[iarg]));
+	}
+	
+	if (files.size() < 2)
+	{
+		std::cout << "HDF filenames missing!\n";
+		return -1;
+	}
+	
+	rArray a;
+	
+	if (not a.hdfload(files[0].c_str()))
+	{
+		std::cout << "Can't open the file \"" << argv[1] << "\"\n";
+		return -1;
+	}
+	
+	if (not a.hdfsave(files[1].c_str(), compress))
+	{
+		std::cout << "Error while writing file \"" << argv[2] << "\"\n";
+		return -1;
+	}
+	
+	return 0;
+}
