90611a16266aace610640fbf3f9a0164e650e7f4
Jakub Benda <jakub.benda@seznam.cz>
2014-04-01 09:53:02 +0200

    ECS: Publish --tolerance switch in program help.

--- hex-ecs/src/input.cpp
+++ hex-ecs/src/input.cpp
@@ -110,10 +110,11 @@ void CommandLine::parse (int argc, char* argv[])
                     "\t--stg-extract             (-c)  only extract amplitudes                                                         \n"
                     "\t--preconditioner <name>   (-p)  preconditioner to use (default: ILU)                                            \n"
                     "\t--list-preconditioners    (-P)  list of available preconditioners with short description of each                \n"
+                    "\t--tolerance <number>      (-t)  tolarance for the conjugate gradients solver                                    \n"
                     "\t--drop-tolerance <number> (-d)  drop tolerance for the ILU preconditioner (default: 1e-15)                      \n"
                     "\t--out-of-core             (-O)  use hard disk drive to store intermediate results and thus to save RAM (slower) \n"
                     "\t--parallel-dot                  OpenMP-parallelize SpMV operations                                              \n"
-                    "\t--no-parallel-block             enable simultaneous preconditioning of multiple blocks by OpenMP                \n"
+                    "\t--no-parallel-block             disable simultaneous preconditioning of multiple blocks by OpenMP               \n"
                     "                                                                                                                  \n"
                 ;
                 exit(0);
@@ -174,7 +175,7 @@ void CommandLine::parse (int argc, char* argv[])
                 droptol = atof(optarg.c_str());
                 return true;
             },
-        "iteration-tolerance", "t", 1, [&](std::string optarg) -> bool
+        "tolerance", "t", 1, [&](std::string optarg) -> bool
             {
                 // iteration tolerance for terminating iteration solution
                 itertol = atof(optarg.c_str());
