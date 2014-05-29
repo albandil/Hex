07fa90847f28dd247d78a754d4479255aa78bc55
Jakub Benda <jakub.benda@seznam.cz>
2014-05-29 20:04:11 +0200

    DWBA: (PWBA:) Skip evaluation of parity-violating contributions.

--- hex-dwba/src/pwba.cpp
+++ hex-dwba/src/pwba.cpp
@@ -15,6 +15,8 @@
 #include "radial.h"
 #include "special.h"
 
+using special::constant::pi;
+
 void pwba
 (
     int Ni, int Li, double ki,
@@ -40,6 +42,10 @@ void pwba
         // for all incoming partial waves
         for (int li = std::abs(Li - L); li <= Li + L; li++)
         {
+            // conserve parity
+            if ((lf + Lf) % 2 != (li + Li) % 2)
+                continue;
+            
             //
             // compute direct contribution
             //
@@ -51,7 +57,7 @@ void pwba
                 double Vdir = direct ? compute_Idir (li, lf, lam, Ni, Li, ki, Nf, Lf, kf) : 0.;
                 
                 // compute complex prefactor
-                Complex prefactor = std::pow((2*M_PI),3) * 8./(ki*kf)*std::pow(Complex(0.,1.),li-lf)/(2.*lam+1)*std::sqrt((2*li+1)/(4*M_PI));
+                Complex prefactor = std::pow(4*pi,3)/(ki*kf)*std::pow(Complex(0.,1.),li-lf)/(2.*lam+1)*std::sqrt((2*li+1)/(4*pi));
                 
                 // for all projections of the initial/final angular momentum
                 for (int Mi = -Li; Mi <= Li; Mi++)
@@ -62,8 +68,6 @@ void pwba
                     
                     // compute angular integrals (Gaunt coefficients)
                     double Gaunts_dir = Gaunt(Li,Mi,lam,Mf-Mi,Lf,Mf) * Gaunt(lam,Mf-Mi,lf,Mi-Mf,li,0);
-                    
-//                     std::cout << "Gaunts_dir = " << Gaunts_dir << std::endl;
                     
                     // add the T-matrix contributions
                     Tdir[idx][lf-std::abs(Lf - L)] += prefactor * Gaunts_dir * Vdir;
@@ -81,7 +85,7 @@ void pwba
                 double Vexc = exchange ? compute_Iexc (li, lf, lam, Ni, Li, ki, Nf, Lf, kf) : 0.;
                 
                 // compute complex prefactor
-                Complex prefactor = std::pow((2*M_PI),3) * 8./(ki*kf)*std::pow(Complex(0.,1.),li-lf)/(2.*lam+1)*std::sqrt((2*li+1)/(4*M_PI));
+                Complex prefactor = std::pow(4*pi,3)/(ki*kf)*std::pow(Complex(0.,1.),li-lf)/(2.*lam+1)*std::sqrt((2*li+1)/(4*pi));
                 
                 // for all projections of the initial/final angular momentum
                 for (int Mi = -Li; Mi <= Li; Mi++)
@@ -92,8 +96,6 @@ void pwba
                     
                     // compute angular integrals (Gaunt coefficients)
                     double Gaunts_exc = Gaunt(li,0,lam,Mf,Lf,Mf) * Gaunt(lam,Mf,lf,Mi-Mf,Li,Mi);
-                    
-//                     std::cout << "Gaunts_exc = " << Gaunts_exc << std::endl;
                     
                     // add the T-matrix contributions
                     Texc[idx][lf-std::abs(Lf - L)] += prefactor * Gaunts_exc * Vexc;
