0e425335e172e95ce0fceb120d361de99c0984ee
Jakub Benda <jakub.benda@seznam.cz>
2015-04-13 22:08:34 +0200

    UTIL: The tool hdf2hdf now converts in both directions (H[ex]DF <-> HDF5).

--- utilities/hdf2hdf/hdf2hdf.cpp
+++ utilities/hdf2hdf/hdf2hdf.cpp
@@ -39,44 +39,24 @@
 
 #include "h5file.cpp"
 
-int main (int argc, char *argv[])
+std::string get_extension (std::string filename)
 {
-    // check number of arguments
-    if (argc == 1 or std::string(argv[0]) == std::string("-h") or std::string(argv[0]) == std::string("--help"))
-    {
-        std::cout << std::endl;
-        std::cout << "Converts files from Hex Data Format (HDF) to Hierarchical Data Format (H5)." << std::endl;
-        std::cout << std::endl;
-        std::cout << "Usage:" << std::endl;
-        std::cout << "\thex-hdf2hdf <srcfile> [<dstfile>]" << std::endl;
-        std::cout << std::endl;
-        std::cout << "If the destination file is not given, the basename of source file will be used with \".h5\" extension." << std::endl;
-        std::cout << std::endl;
-        return argc == 1 ? EXIT_FAILURE : EXIT_SUCCESS;
-    }
+    // skip empty filenames
+    if (filename.size() == 0)
+        return filename;
     
-    // source file name
-    std::string srcfile(argv[1]);
+    // find last dot
+    std::size_t n = filename.size() - 1;
+    for (; n > 0; n--)
+        if (filename[n] == '.')
+            break;
     
-    // destination file name
-    std::string dstfile;
-    if (argc > 2)
-    {
-        dstfile = argv[2];
-    }
-    else
-    {
-        dstfile = argv[1];
-        
-        // erase original extension
-        while (not dstfile.empty() and dstfile.back() != '.')
-            dstfile.pop_back();
-        
-        // append new extension
-        dstfile.push_back('h');
-        dstfile.push_back('5');
-    }
-    
+    // return substring beyond the last dot
+    return filename.substr(n + 1, std::string::npos);
+}
+
+int convert_hdf_to_h5 (std::string srcfile, std::string dstfile)
+{
     // open source file for reading
     HDFFile hdf (srcfile.c_str(), HDFFile::readonly);
     if (not hdf.valid())
@@ -84,9 +64,6 @@ int main (int argc, char *argv[])
         std::cerr << "Cannot open file \"" << srcfile << "\"" << std::endl;
         return EXIT_FAILURE;
     }
-    
-    // mute HDF library
-    H5Eset_auto2(0, 0, nullptr);
     
     // open destination file for writing
     H5File h5f (dstfile.c_str(), H5File::overwrite);
@@ -155,4 +132,125 @@ int main (int argc, char *argv[])
     std::cout << std::endl;
     
     return EXIT_SUCCESS;
+}
+
+herr_t write_dataset_h5_to_hdf (hid_t id, const char * name, const H5L_info_t * info, void * data)
+{
+    H5O_info_t infobuf;
+    if (H5Oget_info_by_name(id, name, &infobuf, H5P_DEFAULT) < 0)
+    {
+        std::cerr << "Failed to determine type of object \"" << name << "\"" << std::endl;
+        return -1;
+    }
+    
+    if (infobuf.type == H5O_TYPE_DATASET)
+        ((std::vector<std::string>*)data)->push_back(name);
+    
+    return 0;
+}
+
+int convert_h5_to_hdf (std::string srcfile, std::string dstfile)
+{
+    // open source file for reading
+    H5File h5f (srcfile.c_str(), H5File::readonly);
+    if (not h5f.valid())
+    {
+        std::cerr << "Cannot open file \"" << srcfile << "\"" << std::endl;
+        return EXIT_FAILURE;
+    }
+    
+    // open destination file for writing
+    HDFFile hdf (dstfile.c_str(), HDFFile::overwrite);
+    if (not hdf.valid())
+    {
+        std::cerr << "Cannot open file \"" << dstfile << "\"" << std::endl;
+        return EXIT_FAILURE;
+    }
+    
+    // get all datasets
+    std::vector<std::string> datasets;
+    if (H5Literate(h5f.file(), H5_INDEX_NAME, H5_ITER_NATIVE, nullptr, &write_dataset_h5_to_hdf, &datasets) < 0)
+    {
+        std::cerr << "Iteration ofer datasets failed." << std::endl;
+        return EXIT_FAILURE;
+    }
+    
+    // copy all datasets
+    for (std::string const & dataset : datasets)
+    {
+        hid_t dset = H5Dopen(h5f.file(), dataset.c_str(), H5P_DEFAULT);
+        hid_t dtyp = H5Dget_type(dset);
+        hid_t dspc = H5Dget_space(dset);
+        
+        hssize_t nElem = H5Sget_simple_extent_npoints(dspc);
+        
+        if (H5Tequal(dtyp, H5T_NATIVE_INT) > 0)
+        {
+            iArray arr(nElem);
+            h5f.read(dataset, &arr[0], nElem);
+            hdf.write(dataset, &arr[0], nElem);
+        }
+        else if (H5Tequal(dtyp, H5T_NATIVE_INT64) > 0)
+        {
+            NumberArray<std::int64_t> arr(nElem);
+            h5f.read(dataset, &arr[0], nElem);
+            hdf.write(dataset, &arr[0], nElem);
+        }
+        else if (H5Tequal(dtyp, H5T_NATIVE_DOUBLE) > 0)
+        {
+            rArray arr(nElem);
+            h5f.read(dataset, &arr[0], nElem);
+            hdf.write(dataset, &arr[0], nElem);
+        }
+        else
+        {
+            std::cerr << "Unknown data type of dataset \"" << dataset << "\"" << std::endl;
+        }
+        
+        H5Sclose(dspc);
+        H5Tclose(dtyp);
+        H5Dclose(dset);
+    }
+    
+    return EXIT_SUCCESS;
+}
+
+int main (int argc, char *argv[])
+{
+    // check number of arguments
+    if (argc < 3)
+    {
+        std::cout << std::endl;
+        std::cout << "Converts files between the Hex Data Format (*.hdf) and the Hierarchical Data Format (*.h5)." << std::endl;
+        std::cout << std::endl;
+        std::cout << "Use" << std::endl;
+        std::cout << "\thex-hdf2hdf <srcfile>.hdf <dstfile>.h5" << std::endl;
+        std::cout << "or" << std::endl;
+        std::cout << "\thex-hdf2hdf <srcfile>.h5  <dstfile>.hdf" << std::endl;
+        std::cout << std::endl;
+        std::cout << "Please note that the Hex data format is flat, and so only flat HDF5 files will be converted successfully." << std::endl;
+        std::cout << "Hex data format also lacks type information. When converting from Hex data format to HDF5, user will be prompted for data types." << std::endl;
+        std::cout << std::endl;
+        return argc == 1 ? EXIT_FAILURE : EXIT_SUCCESS;
+    }
+    
+    // source file name
+    std::string srcfile(argv[1]);
+    std::string srcext = get_extension(srcfile);
+    
+    // destination file name
+    std::string dstfile(argv[2]);
+    std::string dstext = get_extension(dstfile);
+    
+    // mute HDF library
+    H5Eset_auto2(0, 0, nullptr);
+    
+    // pick the conversion direction
+    if (srcext == std::string("hdf") and dstext == std::string("h5"))
+        return convert_hdf_to_h5(srcfile, dstfile);
+    if (srcext == std::string("h5") and dstext == std::string("hdf"))
+        return convert_h5_to_hdf(srcfile, dstfile);
+    
+    std::cout << "The files' format is the same." << std::endl;
+    return EXIT_FAILURE;
 }
