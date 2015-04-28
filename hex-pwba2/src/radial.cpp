d02d338300d59ed7009996ac0080106dff80ad84
Jakub Benda <jakub.benda@seznam.cz>
2015-04-28 12:38:18 +0200

    PWBA2: Remove redundant header.

--- hex-pwba2/src/radial.cpp
+++ hex-pwba2/src/radial.cpp
@@ -29,8 +29,6 @@
 //                                                                                   //
 //  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *  //
 
-#include <mpc.h>
-
 #include <gsl/gsl_interp.h>
 #include <gsl/gsl_spline.h>
 
