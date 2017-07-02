f8fc810622a122ac4d8008e2064d371e56587267
Jakub Benda <jakub.benda@seznam.cz>
2017-07-02 16:12:09 +0200

    ECS: Added test for coupled preconditioner.

---
+++ hex-ecs/tests/25-inner-coupled/runTest.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+rm -f log
+
+hex-ecs --example | tee -a log
+
+mpiexec \
+    -n 4 \
+    -bind-to none \
+    -x OMP_NUM_THREADS=1 \
+    -output-filename log \
+    hex-ecs \
+        --mpi \
+        --groupsize 4 \
+        --shared-scratch \
+        --input example.inp \
+        --preconditioner coupled \
+        --lu mumps \
+    | tee -a log
