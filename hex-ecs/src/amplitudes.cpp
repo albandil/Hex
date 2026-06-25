15284283289bbf2c1441c4248d551865702e47fc
Jakub Benda <jakub.benda@seznam.cz>
2026-06-25 15:52:54 +0200

    Fix transposition of coeff matrix

--- hex-ecs/src/amplitudes.cpp
+++ hex-ecs/src/amplitudes.cpp
@@ -938,7 +938,7 @@ void Amplitudes::computeLambda_ (Amplitudes::Transition T, BlockArray<Complex> &
                 {
                     #pragma omp critical
                     for (int ieig = 0; ieig < Nang; ieig++)
-                        lambda(ieig, i) += 0.5_i * coeff(ieig, ill) * (PsiScFf | WHm[ieig].slice(Nspline_inner, Nspline_full));
+                        lambda(ieig, i) += 0.5_i * coeff(ill, ieig) * (PsiScFf | WHm[ieig].slice(Nspline_inner, Nspline_full));
                 }
                 else
                 {
